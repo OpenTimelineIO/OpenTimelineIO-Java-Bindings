f60a2e1aa28de6f724cfaed2beb7cbd64cde9c23
Karthik Iyer <Karthik.iyer2@yandex.com>
2021-07-18 22:26:32 +0530

    Add linux aarch32 cmake toolchain

---
+++ linux-aarch32-toolchain.cmake
@@ -0,0 +1,6 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)

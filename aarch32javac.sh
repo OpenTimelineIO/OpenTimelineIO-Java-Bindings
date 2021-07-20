f95a75ec2f7388fcf636afa55c3b04a0f577dc16
Karthik Iyer <Karthik.iyer2@yandex.com>
2021-07-21 01:44:42 +0530

    Remove system specific paths

--- aarch32javac.sh
+++ aarch32javac.sh
@@ -1,1 +1,1 @@
-qemu-arm -L /usr/arm-linux-gnueabihf/ /home/karthik/OpenTimelineIO-Java-Bindings/zulu8.54.0.21-ca-jdk8.0.292-linux_aarch32hf/bin/javac "$@"
+qemu-arm -L /usr/arm-linux-gnueabihf/ javac "$@"

6e0c407ef5f1d75c2f99015c1c278d3ac5e77703
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2021-08-23 23:13:31 +0530

    Add support for multiple architectures (#18)
    * Load libraries on mac according to architecture
    
    * Build universal binary on macOS
    
    * Clean up android build command using for each loop
    
    * Add architecture support matrix to README
    
    * Fix 32 bit architecture name
    
    * Add 32-bit support on Linux
    
    * Add 32-bit Ubuntu build to CI
    
    * Remove cmake cache before 32-bit build
    
    * Install multilib compiler before 32-bit build
    
    * Update gradle.yml
    
    * Do not compile native libs while repackaging
    
    * Update architecture support matrix
    
    Add x86 linux support
    
    * Add support for 32-bit windows
    
    * Build for 32-bit windows
    
    * Update gradle.yml
    
    * Rerun CI
    
    * Clear cmake cache
    
    * Fix Win32 build
    
    * Update architecture support matrix
    
    - Add windows x86 support
    - Mark macOS x86 support as deprecated
    
    * Cleanup Linux build commands
    
    * Fix linux build
    
    * Add build commands for aarch64 linux
    
    * Build for aarch64 Linux
    
    * Update gradle.yml
    
    * Fix jdk name
    
    * Update architecture support matrix
    
    - Add support for aarch64 Linux
    
    * Add build commands for aarch32 Linux
    
    * Build for aarch32 Linux
    
    * Update gradle.yml
    
    * Add linux aarch32 cmake toolchain
    
    * Update architecture support matrix
    
    Add support for aarch32 Linux
    
    * Minor change in modified executable
    
    * Add commands to build for arm32 and arm64 windows
    
    * Load arm libs on windows
    
    * Build for arm64 and arm32 Windows
    
    Note: arm for windows builds are not tested
    
    * Update architecture support matrix
    
    Add experimental untested support for ARM Windows
    
    * Fix architecture support matrix
    
    * Fix arch name in LibraryLoader
    
    * Simplify platform name code in LibraryLoader
    
    * Simplify conditional
    
    * Remove system specific paths
    
    * Remove bash scripts and create them through gradle
    
    * Add bash scripts to gitignore
    
    * Simplify creation of modifies java executable
    
    * Use architecture matrix in CI
    
    * Create android.yml
    
    * Do not install smake in combine job
    
    * Remove jcenter deps
    
    * Use AdoptOpenJDK builds for testing arm build
    
    * Log info
    
    * Ignore aarch32 ubuntu
    
    * Install qemu for aacrh32 build
    
    * Disable verbose output in CI

---
+++ linux-aarch64-toolchain.cmake
@@ -0,0 +1,6 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
